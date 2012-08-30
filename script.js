19715d1b8ceb9a92dad715144e0414ebab991e39
Okal Otieno <okal@justokal.com>
2012-08-30 22:45:03 +0300

    Add function to grab reading time via AJAX

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+var getReadingTime = function(info, tab) {
+    alert("Constructing request");
+    var req = XMLHttpRequest();
+
+    req.open(
+        "GET",
+        "http://www.reading-time.samrat.me/api?url="+
+           info.linkUrl,
+        true);
+    req.onload = function() {
+        var data = eval(req.responseText);
+        alert(data.readable);
+    }
+    req.send(null);
+}
+
 var menuItem = chrome.contextMenus.create({
     "title": "How long will this take?",
+    "contexts": ["link"],
     "onclick": getReadingTime
 });
