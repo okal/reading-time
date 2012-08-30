9fc9da3cbc7a6ce57a25ca74fba564e7c8c96e54
Okal Otieno <okal@justokal.com>
2012-08-31 01:57:12 +0300

    Inject custom CSS files

--- custom_style.js
+++ custom_style.js
@@ -1,5 +1,10 @@
-var style = document.createElement('link');
-style.rel = 'stylesheet';
-style.type = 'text/css';
-style.href = chrome.extension.getURL('custom_style.css');
-document.head.appendChild(style);
+var injectStyle = function(href) {
+    var style = document.createElement('style');
+    style.rel = 'stylesheet';
+    style.type = 'text/css';
+    style.href = href;
+    document.head.appendChild(style);
+}
+
+injectStyle(chrome.extension.getURL('smoke.css'));
+injectStyle(chrome.extension.getURL('cheetah.css'));
