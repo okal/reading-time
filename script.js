d40f1111e342378b15a2b6fa0df5d5297f073f62
Okal Otieno <okal@justokal.com>
2012-08-31 01:58:58 +0300

    Complete reading time alert system

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-var getReadingTime = function(info, tab) {
-    alert("Constructing request");
-    var req = XMLHttpRequest();
-
+function getReadingTime(info, tab) {
+    var timeUrl = "http://www.reading-time.samrat.me/api?url="+info.linkUrl;
+    var req = new XMLHttpRequest();
     req.open(
         "GET",
-        "http://www.reading-time.samrat.me/api?url="+
-           info.linkUrl,
-        true);
-    req.onload = function() {
-        var data = eval(req.responseText);
-        alert(data.readable);
-    }
+        timeUrl,
+        true
+    );
+    req.onload = showTime;
     req.send(null);
+
+    function showTime() {
+        var readableTime = JSON.parse(req.responseText).readable;
+        var code = 'smoke.signal("This will take '+readableTime+'");'
+        chrome.tabs.executeScript(tab.id, {'code': code});
+    }
 }
 
 var menuItem = chrome.contextMenus.create({
